32b6cdd3c9108b2299692230a45263ec078c0dd0
estresadinha <120022356+estresadinha@users.noreply.github.com>
2023-01-25 11:54:39 -0300

    Add files via upload

---
+++ Projeto19Janeiro/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace Projeto18Janeiro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtResultado = new System.Windows.Forms.TextBox();
+            this.labelOperacao = new System.Windows.Forms.Label();
+            this.botao7 = new System.Windows.Forms.Button();
+            this.botao8 = new System.Windows.Forms.Button();
+            this.botao9 = new System.Windows.Forms.Button();
+            this.botao6 = new System.Windows.Forms.Button();
+            this.botao5 = new System.Windows.Forms.Button();
+            this.botao4 = new System.Windows.Forms.Button();
+            this.botao3 = new System.Windows.Forms.Button();
+            this.botao2 = new System.Windows.Forms.Button();
+            this.botao1 = new System.Windows.Forms.Button();
+            this.botaoSoma = new System.Windows.Forms.Button();
+            this.botaoSub = new System.Windows.Forms.Button();
+            this.botaoMult = new System.Windows.Forms.Button();
+            this.botaoDiv = new System.Windows.Forms.Button();
+            this.botao0 = new System.Windows.Forms.Button();
+            this.botaoCE = new System.Windows.Forms.Button();
+            this.botaoC = new System.Windows.Forms.Button();
+            this.botaoResult = new System.Windows.Forms.Button();
+            this.botaoPonto = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtResultado
+            // 
+            this.TxtResultado.Location = new System.Drawing.Point(12, 12);
+            this.TxtResultado.Name = "TxtResultado";
+            this.TxtResultado.Size = new System.Drawing.Size(322, 20);
+            this.TxtResultado.TabIndex = 0;
+            this.TxtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // labelOperacao
+            // 
+            this.labelOperacao.AutoSize = true;
+            this.labelOperacao.BackColor = System.Drawing.Color.Transparent;
+            this.labelOperacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOperacao.Location = new System.Drawing.Point(12, 12);
+            this.labelOperacao.Name = "labelOperacao";
+            this.labelOperacao.Size = new System.Drawing.Size(84, 20);
+            this.labelOperacao.TabIndex = 1;
+            this.labelOperacao.Text = "operacao";
+            // 
+            // botao7
+            // 
+            this.botao7.BackColor = System.Drawing.Color.Silver;
+            this.botao7.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao7.ForeColor = System.Drawing.Color.Black;
+            this.botao7.Location = new System.Drawing.Point(13, 37);
+            this.botao7.Name = "botao7";
+            this.botao7.Size = new System.Drawing.Size(59, 57);
+            this.botao7.TabIndex = 2;
+            this.botao7.Text = "7";
+            this.botao7.UseVisualStyleBackColor = false;
+            this.botao7.Click += new System.EventHandler(this.botao7_Click);
+            // 
+            // botao8
+            // 
+            this.botao8.BackColor = System.Drawing.Color.Silver;
+            this.botao8.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.botao8.Location = new System.Drawing.Point(78, 37);
+            this.botao8.Name = "botao8";
+            this.botao8.Size = new System.Drawing.Size(59, 57);
+            this.botao8.TabIndex = 3;
+            this.botao8.Text = "8";
+            this.botao8.UseVisualStyleBackColor = false;
+            this.botao8.Click += new System.EventHandler(this.botao8_Click);
+            // 
+            // botao9
+            // 
+            this.botao9.BackColor = System.Drawing.Color.Silver;
+            this.botao9.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.botao9.Location = new System.Drawing.Point(143, 37);
+            this.botao9.Name = "botao9";
+            this.botao9.Size = new System.Drawing.Size(59, 57);
+            this.botao9.TabIndex = 4;
+            this.botao9.Text = "9";
+            this.botao9.UseVisualStyleBackColor = false;
+            this.botao9.Click += new System.EventHandler(this.botao9_Click);
+            // 
+            // botao6
+            // 
+            this.botao6.BackColor = System.Drawing.Color.Silver;
+            this.botao6.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.botao6.Location = new System.Drawing.Point(143, 100);
+            this.botao6.Name = "botao6";
+            this.botao6.Size = new System.Drawing.Size(59, 57);
+            this.botao6.TabIndex = 7;
+            this.botao6.Text = "6";
+            this.botao6.UseVisualStyleBackColor = false;
+            this.botao6.Click += new System.EventHandler(this.botao6_Click);
+            // 
+            // botao5
+            // 
+            this.botao5.BackColor = System.Drawing.Color.Silver;
+            this.botao5.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.botao5.Location = new System.Drawing.Point(78, 100);
+            this.botao5.Name = "botao5";
+            this.botao5.Size = new System.Drawing.Size(59, 57);
+            this.botao5.TabIndex = 6;
+            this.botao5.Text = "5";
+            this.botao5.UseVisualStyleBackColor = false;
+            this.botao5.Click += new System.EventHandler(this.botao5_Click);
+            // 
+            // botao4
+            // 
+            this.botao4.BackColor = System.Drawing.Color.Silver;
+            this.botao4.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.botao4.Location = new System.Drawing.Point(13, 100);
+            this.botao4.Name = "botao4";
+            this.botao4.Size = new System.Drawing.Size(59, 57);
+            this.botao4.TabIndex = 5;
+            this.botao4.Text = "4";
+            this.botao4.UseVisualStyleBackColor = false;
+            this.botao4.Click += new System.EventHandler(this.botao4_Click);
+            // 
+            // botao3
+            // 
+            this.botao3.BackColor = System.Drawing.Color.Silver;
+            this.botao3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.botao3.Location = new System.Drawing.Point(143, 161);
+            this.botao3.Name = "botao3";
+            this.botao3.Size = new System.Drawing.Size(59, 57);
+            this.botao3.TabIndex = 10;
+            this.botao3.Text = "3";
+            this.botao3.UseVisualStyleBackColor = false;
+            this.botao3.Click += new System.EventHandler(this.botao3_Click);
+            // 
+            // botao2
+            // 
+            this.botao2.BackColor = System.Drawing.Color.Silver;
+            this.botao2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.botao2.Location = new System.Drawing.Point(78, 161);
+            this.botao2.Name = "botao2";
+            this.botao2.Size = new System.Drawing.Size(59, 57);
+            this.botao2.TabIndex = 9;
+            this.botao2.Text = "2";
+            this.botao2.UseVisualStyleBackColor = false;
+            this.botao2.Click += new System.EventHandler(this.botao2_Click);
+            // 
+            // botao1
+            // 
+            this.botao1.BackColor = System.Drawing.Color.Silver;
+            this.botao1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.botao1.Location = new System.Drawing.Point(13, 161);
+            this.botao1.Name = "botao1";
+            this.botao1.Size = new System.Drawing.Size(59, 57);
+            this.botao1.TabIndex = 8;
+            this.botao1.Text = "1";
+            this.botao1.UseVisualStyleBackColor = false;
+            this.botao1.Click += new System.EventHandler(this.botao1_Click);
+            // 
+            // botaoSoma
+            // 
+            this.botaoSoma.BackColor = System.Drawing.Color.Silver;
+            this.botaoSoma.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.botaoSoma.Location = new System.Drawing.Point(208, 37);
+            this.botaoSoma.Name = "botaoSoma";
+            this.botaoSoma.Size = new System.Drawing.Size(59, 57);
+            this.botaoSoma.TabIndex = 11;
+            this.botaoSoma.Text = "+";
+            this.botaoSoma.UseVisualStyleBackColor = false;
+            this.botaoSoma.Click += new System.EventHandler(this.botaoSoma_Click);
+            // 
+            // botaoSub
+            // 
+            this.botaoSub.BackColor = System.Drawing.Color.Silver;
+            this.botaoSub.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.botaoSub.Location = new System.Drawing.Point(208, 100);
+            this.botaoSub.Name = "botaoSub";
+            this.botaoSub.Size = new System.Drawing.Size(59, 57);
+            this.botaoSub.TabIndex = 12;
+            this.botaoSub.Text = "-";
+            this.botaoSub.UseVisualStyleBackColor = false;
+            this.botaoSub.Click += new System.EventHandler(this.botaoSub_Click);
+            // 
+            // botaoMult
+            // 
+            this.botaoMult.BackColor = System.Drawing.Color.Silver;
+            this.botaoMult.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.botaoMult.Location = new System.Drawing.Point(208, 163);
+            this.botaoMult.Name = "botaoMult";
+            this.botaoMult.Size = new System.Drawing.Size(59, 57);
+            this.botaoMult.TabIndex = 13;
+            this.botaoMult.Text = "x";
+            this.botaoMult.UseVisualStyleBackColor = false;
+            this.botaoMult.Click += new System.EventHandler(this.botaoMult_Click);
+            // 
+            // botaoDiv
+            // 
+            this.botaoDiv.BackColor = System.Drawing.Color.Silver;
+            this.botaoDiv.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.botaoDiv.Location = new System.Drawing.Point(208, 224);
+            this.botaoDiv.Name = "botaoDiv";
+            this.botaoDiv.Size = new System.Drawing.Size(59, 57);
+            this.botaoDiv.TabIndex = 14;
+            this.botaoDiv.Text = "/";
+            this.botaoDiv.UseVisualStyleBackColor = false;
+            this.botaoDiv.Click += new System.EventHandler(this.botaoDiv_Click);
+            // 
+            // botao0
+            // 
+            this.botao0.BackColor = System.Drawing.Color.Silver;
+            this.botao0.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.botao0.Location = new System.Drawing.Point(13, 224);
+            this.botao0.Name = "botao0";
+            this.botao0.Size = new System.Drawing.Size(124, 57);
+            this.botao0.TabIndex = 15;
+            this.botao0.Text = "0";
+            this.botao0.UseVisualStyleBackColor = false;
+            this.botao0.Click += new System.EventHandler(this.botao0_Click);
+            // 
+            // botaoCE
+            // 
+            this.botaoCE.BackColor = System.Drawing.Color.Silver;
+            this.botaoCE.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.botaoCE.Location = new System.Drawing.Point(273, 37);
+            this.botaoCE.Name = "botaoCE";
+            this.botaoCE.Size = new System.Drawing.Size(59, 57);
+            this.botaoCE.TabIndex = 16;
+            this.botaoCE.Text = "CE";
+            this.botaoCE.UseVisualStyleBackColor = false;
+            this.botaoCE.Click += new System.EventHandler(this.botaoCE_Click);
+            // 
+            // botaoC
+            // 
+            this.botaoC.BackColor = System.Drawing.Color.Silver;
+            this.botaoC.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.botaoC.Location = new System.Drawing.Point(273, 100);
+            this.botaoC.Name = "botaoC";
+            this.botaoC.Size = new System.Drawing.Size(59, 57);
+            this.botaoC.TabIndex = 17;
+            this.botaoC.Text = "C";
+            this.botaoC.UseVisualStyleBackColor = false;
+            this.botaoC.Click += new System.EventHandler(this.botaoC_Click);
+            // 
+            // botaoResult
+            // 
+            this.botaoResult.BackColor = System.Drawing.Color.Silver;
+            this.botaoResult.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.botaoResult.Location = new System.Drawing.Point(273, 163);
+            this.botaoResult.Name = "botaoResult";
+            this.botaoResult.Size = new System.Drawing.Size(59, 118);
+            this.botaoResult.TabIndex = 18;
+            this.botaoResult.Text = "=";
+            this.botaoResult.UseVisualStyleBackColor = false;
+            this.botaoResult.Click += new System.EventHandler(this.botaoResult_Click);
+            // 
+            // botaoPonto
+            // 
+            this.botaoPonto.BackColor = System.Drawing.Color.Silver;
+            this.botaoPonto.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold);
+            this.botaoPonto.Location = new System.Drawing.Point(143, 224);
+            this.botaoPonto.Name = "botaoPonto";
+            this.botaoPonto.Size = new System.Drawing.Size(59, 57);
+            this.botaoPonto.TabIndex = 19;
+            this.botaoPonto.Text = ".";
+            this.botaoPonto.UseVisualStyleBackColor = false;
+            this.botaoPonto.Click += new System.EventHandler(this.botaoPonto_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 305);
+            this.Controls.Add(this.botaoPonto);
+            this.Controls.Add(this.botaoResult);
+            this.Controls.Add(this.botaoC);
+            this.Controls.Add(this.botaoCE);
+            this.Controls.Add(this.botao0);
+            this.Controls.Add(this.botaoDiv);
+            this.Controls.Add(this.botaoMult);
+            this.Controls.Add(this.botaoSub);
+            this.Controls.Add(this.botaoSoma);
+            this.Controls.Add(this.botao3);
+            this.Controls.Add(this.botao2);
+            this.Controls.Add(this.botao1);
+            this.Controls.Add(this.botao6);
+            this.Controls.Add(this.botao5);
+            this.Controls.Add(this.botao4);
+            this.Controls.Add(this.botao9);
+            this.Controls.Add(this.botao8);
+            this.Controls.Add(this.botao7);
+            this.Controls.Add(this.labelOperacao);
+            this.Controls.Add(this.TxtResultado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtResultado;
+        private System.Windows.Forms.Label labelOperacao;
+        private System.Windows.Forms.Button botao7;
+        private System.Windows.Forms.Button botao8;
+        private System.Windows.Forms.Button botao9;
+        private System.Windows.Forms.Button botao6;
+        private System.Windows.Forms.Button botao5;
+        private System.Windows.Forms.Button botao4;
+        private System.Windows.Forms.Button botao3;
+        private System.Windows.Forms.Button botao2;
+        private System.Windows.Forms.Button botao1;
+        private System.Windows.Forms.Button botaoSoma;
+        private System.Windows.Forms.Button botaoSub;
+        private System.Windows.Forms.Button botaoMult;
+        private System.Windows.Forms.Button botaoDiv;
+        private System.Windows.Forms.Button botao0;
+        private System.Windows.Forms.Button botaoCE;
+        private System.Windows.Forms.Button botaoC;
+        private System.Windows.Forms.Button botaoResult;
+        private System.Windows.Forms.Button botaoPonto;
+    }
+}
+
---
+++ Projeto19Janeiro/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto18Janeiro
+{
+    public partial class Form1 : Form
+    {
+        decimal valor1 = 0;
+        decimal valor2 = 0;
+        string operacao = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void botao7_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + "7";
+        }
+
+
+        private void botao8_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + "8";
+
+        }
+
+        private void botao9_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + "9";
+
+        }
+
+        private void botao4_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + "4";
+        }
+
+        private void botao5_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + "5";
+        }
+
+        private void botao6_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + "6";
+        }
+
+        private void botao1_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + "1";
+        }
+
+        private void botao2_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + "2";
+        }
+
+        private void botao3_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + "3";
+        }
+
+        private void botao0_Click(object sender, EventArgs e)
+        {
+            TxtResultado.Text = TxtResultado.Text + "0";
+        }
+
+
+
+        private void botaoResult_Click(object sender, EventArgs e)
+
+        {
+            if (TxtResultado.Text != "")
+            {
+
+                valor2 = decimal.Parse(TxtResultado.Text, CultureInfo.InvariantCulture);
+                if (operacao == "soma")
+                {
+                    TxtResultado.Text = Convert.ToString(valor1 + valor2);
+                }
+
+                if (operacao == "sub")
+                {
+                    TxtResultado.Text = Convert.ToString(valor1 - valor2);
+                }
+
+                if (operacao == "div")
+                {
+                    if (valor1 < valor2)
+                    {
+                        TxtResultado.ForeColor = Color.Red;
+                        TxtResultado.Text = "Inválido";
+                    }
+                    else
+                        TxtResultado.Text = Convert.ToString(valor1 / valor2);
+
+                }
+
+
+                if (operacao == "mult")
+                {
+                    TxtResultado.Text = Convert.ToString(valor1 * valor2);
+
+                }
+               
+            }
+            else
+                MessageBox.Show("digite um valor para efetuar uma operação");
+        }
+
+        private void botaoSoma_Click(object sender, EventArgs e)
+        {
+            if (TxtResultado.Text != "") {
+                valor1 = decimal.Parse(TxtResultado.Text, CultureInfo.InvariantCulture);
+                TxtResultado.Text = "";
+                operacao = "soma";
+                labelOperacao.Text = "+";
+            }
+            else
+                MessageBox.Show("favor digitar um número");
+
+
+
+
+        }
+
+
+        private void botaoSub_Click(object sender, EventArgs e)
+        {
+            if (TxtResultado.Text != "")
+            {
+
+                valor1 = decimal.Parse(TxtResultado.Text, CultureInfo.InvariantCulture);
+                TxtResultado.Text = "";
+                operacao = "sub";
+                labelOperacao.Text = "-";
+
+            }
+            else
+                MessageBox.Show("favor digitar um número");
+        }
+         
+             
+
+
+
+            private void botaoMult_Click(object sender, EventArgs e)
+            {
+            if (TxtResultado.Text != "")
+            {
+
+                valor1 = decimal.Parse(TxtResultado.Text, CultureInfo.InvariantCulture);
+                TxtResultado.Text = "";
+                operacao = "mult";
+                labelOperacao.Text = "*";
+
+            }
+            else
+                MessageBox.Show("favor digitar um número");
+
+        }
+
+
+
+            private void botaoDiv_Click(object sender, EventArgs e)
+            {
+            if (TxtResultado.Text != "")
+            {
+
+                valor1 = decimal.Parse(TxtResultado.Text, CultureInfo.InvariantCulture);
+                TxtResultado.Text = "";
+                operacao = "div";
+                labelOperacao.Text = "/";
+
+            }
+
+            else
+                MessageBox.Show("favor digitar um número");
+        }
+
+
+            private void botaoCE_Click(object sender, EventArgs e)
+            {
+                TxtResultado.Text = "";
+
+            }
+
+            private void botaoC_Click(object sender, EventArgs e)
+            {
+                TxtResultado.Text = "";
+                valor1 = 0;
+                valor2 = 0;
+                labelOperacao.Text = "";
+            }
+
+            private void botaoPonto_Click(object sender, EventArgs e)
+            {
+            TxtResultado.Text = TxtResultado.Text + ".";
+            }
+        
+
+
+    }
+}
